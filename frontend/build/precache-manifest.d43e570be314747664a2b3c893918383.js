415760ff0fd7b1fcfa3fbed93810bf4fed76ec19
Berk Çapar <berkcapar@gmail.com>
2020-09-08 13:53:43 +0300

    proxy added

---
+++ frontend/build/precache-manifest.d43e570be314747664a2b3c893918383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a0340e06e12679a1c809e4df08bf3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd891fac3a95c6c4125b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc135a18ebafc7ad8dee",
+    "url": "/static/js/2.f9392ec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9392ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd891fac3a95c6c4125b",
+    "url": "/static/js/main.4ead77e7.chunk.js"
+  },
+  {
+    "revision": "3d58d0c924ce281a9d53",
+    "url": "/static/js/runtime-main.74e1abef.js"
+  }
+]);
